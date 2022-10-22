86b5794921fb6a54a07aaf83fc24da638930ce14
gaowanlu <2209120827@qq.com>
2022-10-23 02:34:10 +0800

    init rep

---
+++ src/api/login.js
@@ -0,0 +1,18 @@
+import request from '@/utils/request'
+
+export function reqLogin(data) {
+  console.log("登录");
+  return request({
+    url: '/auth/signin',
+    method: 'post',
+    data
+  })
+}
+
+export function reqLogout(data) {
+  return request({
+    url: '/logout',
+    method: 'post',
+    data
+  })
+}
---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { getUserInfo } from "@/store/actions";
+import Layout from "@/views/layout";
+import Login from "@/views/login";
+class Router extends React.Component {
+  render() {
+    const { token, role, getUserInfo } = this.props;
+    return (
+      <HashRouter>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route
+            path="/"
+            render={() => {
+              if (!token) {
+                return <Redirect to="/login" />;
+              } else {
+                if (role) {
+                  return <Layout />;
+                } else {
+                  console.log("router/index.js 获取个人身份信息",token);
+                  getUserInfo(token).then(() => <Layout />).catch((e) => { alert("router/index.js 获取个人信息失败"); });
+                }
+              }
+            }}
+          />
+        </Switch>
+      </HashRouter>
+    );
+  }
+}
+
+export default connect((state) => state.user, { getUserInfo })(Router);
